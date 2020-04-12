935739a54cdb96df8ec5da4b6940e6ad0d95d989
litvinov.dima2001@yandex.ru <litvinov.dima2001@yandex.ru>
2020-04-13 01:11:32 +0300

    Добавьте файлы проекта.

---
+++ Fich/ProcessSortSearch.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Fich
+{
+    internal class ProcessSortSearch
+    {
+        const int N = 10;
+
+        //------------SortAsc
+        internal static void SortAsc(BindingList<CoTrigonometric> bList) 
+        {
+            CoTrigonometric ct;
+            for (int j = 0; j < N; j++)
+            {
+                for (int i = 0; i < N - 1; i++) 
+                {
+                    if (bList[i].abs > bList[i + 1].abs)
+                    {
+                        ct = bList[i];
+                        bList[i] = bList[i + 1];
+                        bList[i + 1] = ct;
+                    }
+
+                }
+
+                for (int i = 0; i < N - 1; i++) 
+                {
+                    if (bList[i].abs == bList[i + 1].abs && bList[i].fi > bList[i + 1].fi)
+                    {
+                        ct = bList[i];
+                        bList[i] = bList[i + 1];
+                        bList[i + 1] = ct;
+                    }
+
+                }
+
+            }
+
+        }
+        //------------SearchLinear
+        internal static bool SearchLinear(BindingList<CoTrigonometric> bList, double abs, double fi, double eps) 
+        {
+            foreach (CoTrigonometric c in bList)
+            {
+                if (Math.Abs(c.abs - abs) < eps && Math.Abs(c.fi - fi) < eps)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        internal static bool SearchLinear(BindingList<CoTrigonometric> bList, CoTrigonometric ct , double eps)
+        {
+            foreach (CoTrigonometric c in bList)
+            {
+                if (Math.Abs(c.abs - ct.abs) < eps && Math.Abs(c.fi - ct.fi) < eps)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        //------------------SearchBinary
+
+        internal static bool SearchBinary(BindingList<CoTrigonometric> bList, double abs, double fi, double eps, int l, int r) 
+        {
+
+            if (r >= l)
+            {
+                int mid = l + (r - l) / 2;
+
+                if (Math.Abs(bList[mid].abs - abs) < eps && Math.Abs(bList[mid].fi - fi) < eps)
+                    return true;
+
+                if (bList[mid].abs > abs || bList[mid].fi > fi)
+                    return SearchBinary(bList, abs, fi, eps, l, mid - 1);
+
+                return SearchBinary(bList, abs, fi, eps, mid + 1, r);
+            }
+
+            return false;
+        }
+
+        internal static bool SearchBinary(BindingList<CoTrigonometric> bList, CoTrigonometric ct, double eps, int l, int r)
+        {
+
+            if (r >= l)
+            {
+                int mid = l + (r - l) / 2;
+
+                if (Math.Abs(bList[mid].abs - ct.abs) < eps && Math.Abs(bList[mid].fi - ct.fi) < eps)
+                    return true;
+
+                if (bList[mid].abs > ct.abs || bList[mid].fi > ct.fi)
+                    return SearchBinary(bList, ct, eps, l, mid - 1);
+
+                return SearchBinary(bList, ct, eps, mid + 1, r);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Fich/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Fich
+{
+    class Program
+    {
+        const int N = 10;
+        static void Main(string[] args)
+        {
+            BindingList<CoTrigonometric> bList = new BindingList<CoTrigonometric>();
+
+            CoTrigonometric ct;
+            Random x = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                ct = new CoTrigonometric(x.Next(10), x.Next(10));
+                bList.Add(ct);
+            }
+            //ct = new CoTrigonometric(1, 1);
+            //bList.Add(ct);
+
+            foreach (CoTrigonometric c in bList)
+            {
+                Console.WriteLine(c.abs + " " + c.fi);
+            }
+            Console.WriteLine();
+
+            //------------------input data
+
+            double eps = 0.1;
+            double abs = 1;
+            double fi = 1;
+            ct = new CoTrigonometric(1, 1);
+
+            //------------------sort
+
+            ProcessSortSearch.SortAsc(bList);
+
+            foreach (CoTrigonometric c in bList)
+            {
+                Console.WriteLine(c.abs + " " + c.fi);
+            }
+            Console.WriteLine();
+
+            //------------SearchLinear
+
+            if (ProcessSortSearch.SearchLinear(bList, abs, fi, eps))
+            {
+                Console.WriteLine("YES");
+            }
+            else { Console.WriteLine("NO"); }
+
+            if (ProcessSortSearch.SearchLinear(bList, ct, eps))
+            {
+                Console.WriteLine("YES");
+            }
+            else { Console.WriteLine("NO"); }
+
+            //------------SearchBinary
+
+            int length = bList.Count;
+
+            if (ProcessSortSearch.SearchBinary(bList, abs, fi, eps, 0, length - 1)) 
+            {
+                Console.WriteLine("YES");
+            }
+            else { Console.WriteLine("NO"); }
+
+            if (ProcessSortSearch.SearchBinary(bList, ct, eps, 0, length - 1))
+            {
+                Console.WriteLine("YES");
+            }
+            else { Console.WriteLine("NO"); }
+
+        }
+    }
+}
